5c88123b6f4edf549189562b448333940fd975f6
Alexander A. Klimov <alexander.klimov@icinga.com>
2025-09-15 16:49:13 +0200

    Auto-update

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,12 @@
   networking.hostName = "aklimov-nixos";
   system.stateVersion = "23.11";
   boot.tmp.cleanOnBoot = true;
+
   networking.firewall.enable = true;
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 7d";
 
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBIroHYGSRaRNFxlK90SS0aHwWjEME30pK5J1N/V1w6a" ];
