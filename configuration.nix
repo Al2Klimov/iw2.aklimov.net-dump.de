8d2e41e422d352039f84485e08520e1641f08bff
Alexander A. Klimov <alexander.klimov@icinga.com>
2025-09-16 10:17:59 +0200

    Icinga Web 2

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -8,6 +8,8 @@
   boot.tmp.cleanOnBoot = true;
 
   networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 80 ];
+
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
   nix.gc.automatic = true;
@@ -15,4 +17,45 @@
 
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBIroHYGSRaRNFxlK90SS0aHwWjEME30pK5J1N/V1w6a" ];
+
+  services.mysql = with pkgs; {
+    enable = true;
+    package = mariadb;
+    initialDatabases = [
+      {
+        name = "iw2";
+        schema = writeText "schema" ((builtins.readFile "${icingaweb2.src}/schema/mysql.schema.sql") + ''
+INSERT INTO icingaweb_user VALUES ('icingaadmin', 1, '$2y$05$bZFogtHKoarFf3QMSLs8.eBfgDdSBA/ULt0SEQOEIkcg0/gvs9MuW', NOW(), NOW());
+'');
+      }
+    ];
+    ensureUsers = [
+      {
+        name = "icingaweb2";
+        ensurePermissions."iw2.*" = "ALL PRIVILEGES";
+      }
+    ];
+  };
+
+  services.icingaweb2 = {
+    enable = true;
+    generalConfig.global.config_resource = "iw2";
+    modules.monitoring.enable = false;
+    authentications.mysql = {
+      backend = "db";
+      resource = "iw2";
+    };
+    resources.iw2 = {
+      type = "db";
+      db = "mysql";
+      host = "localhost";
+      dbname = "iw2";
+      username = "icingaweb2";
+      charset = "utf8";
+    };
+    roles.adm = {
+      users = "icingaadmin";
+      permissions = "*";
+    };
+  };
 }
